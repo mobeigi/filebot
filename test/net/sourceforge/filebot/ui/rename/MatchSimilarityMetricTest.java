caef14cfa4c4fcfd32e2d978925494175d43c299
Reinhard Pointner <rednoah@users.noreply.github.com>
2011-11-13 06:33:54 +0000

    * docs

--- test/net/sourceforge/filebot/ui/rename/MatchSimilarityMetricTest.java
+++ test/net/sourceforge/filebot/ui/rename/MatchSimilarityMetricTest.java
@@ -9,9 +9,24 @@ import java.io.File;
 
 import org.junit.Test;
 
+import net.sourceforge.filebot.web.Date;
+import net.sourceforge.filebot.web.Episode;
+
 
 public class MatchSimilarityMetricTest {
 	
+	@Test
+	public void substringMetrics() {
+		Episode eY1T1 = new Episode("Doctor Who", new Date(2005, 0, 0), 1, 1, "Rose");
+		Episode eY2T2 = new Episode("Doctor Who", new Date(1963, 0, 0), 1, 1, "An Unearthly Child");
+		File fY1T1 = new File("Doctor Who (2005)/Doctor Who - 1x01 - Rose");
+		File fY2T2 = new File("Doctor Who (1963)/Doctor Who - 1x01 - An Unearthly Child");
+		
+		assertEquals(3.0 / 3, Title.getSimilarity(eY1T1, fY1T1), 0);
+		assertEquals(2.0 / 3, Title.getSimilarity(eY1T1, fY2T2), 0.01);
+	}
+	
+
 	@Test
 	public void nameIgnoreEmbeddedChecksum() {
 		assertEquals(1, Name.getSimilarity("test", "test [EF62DF13]"), 0);
