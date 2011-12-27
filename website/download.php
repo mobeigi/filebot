30e582b02e5e4e623278015079e2404fbb12ea33
Reinhard Pointner <rednoah@users.noreply.github.com>
2011-12-27 06:03:17 +0000

    * download redirect script for the latest version of a given package

---
+++ website/download.php
@@ -0,0 +1,21 @@
+<?php
+$version = isset($_GET['version']) ? $_GET['version'] : '@{version}';
+$arch = $_GET['arch'];
+$type = $_GET['type'];
+
+$folder = 'http://sourceforge.net/projects/filebot/files/filebot/FileBot_'.$version;
+$file = 'undefined';
+if ($type == 'msi')
+	$file = 'FileBot_'.$version.'_'.$arch.'.msi';
+if ($type == 'deb')
+	$file =  'filebot_'.$version.'_'.$arch.'.deb';
+if ($type == 'app')
+	$file = 'FileBot_'.$version.'.app.tar.gz';
+if ($type == 'jar')
+	$file = 'FileBot_'.$version.'.jar';
+if ($type == 'src')
+	$file = 'filebot-'.$version.'-src.zip';
+
+header('HTTP/1.1 302 Found');
+header('Location: '.$folder.'/'.$file.'/download');
+?>
