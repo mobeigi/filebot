4d3c2c6f55a5f620e44772944f7d7d42f216d8e4
Reinhard Pointner <rednoah@users.noreply.github.com>
2012-02-23 09:02:16 +0000

    * fixfix

--- website/scripts/cpmv.groovy
+++ website/scripts/cpmv.groovy
@@ -5,8 +5,5 @@ def temp = args*.copyTo("E:/filebot-temp")
 
 // sort copies into the given file structure
 temp.eachMediaFolder { 
-	rename(folder:it, format:'E:/TV Shows/{n}/{n} - {s00e00} - {t}', db:'thetvdb')
+	rename(folder:it, format:'E:/TV Shows/{n}/{n} - {s00e00} - {t}', db:'thetvdb', strict:false)
 }
-
-// delete temporary folders
-temp*.deleteDir()
