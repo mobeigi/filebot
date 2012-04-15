4479b123d5f933ce93b050312fdf57c33efd8f1e
Reinhard Pointner <rednoah@users.noreply.github.com>
2012-04-15 15:44:19 +0000

    * add -Dunixfs=false as default value to all unix startup scripts

--- installer/deb/filebot.sh
+++ installer/deb/filebot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Dapplication.deployment=deb -Djna.library.path=/usr/share/filebot -Djava.library.path=/usr/share/filebot -Xmx256m -jar /usr/share/filebot/FileBot.jar "$@"
+java -Dunixfs=false -Xmx256m -Dapplication.deployment=deb -Djna.library.path=/usr/share/filebot -Djava.library.path=/usr/share/filebot -jar /usr/share/filebot/FileBot.jar "$@"
