861d557e9228d73e54b53bac9b6e6162f1c89121
Reinhard Pointner <rednoah@users.noreply.github.com>
2012-07-08 12:38:34 +0000

    * make {vf} only evaluate to standard tags like 1080p, 720p, 480p * the old {vf} binding is now {hpi}

--- source/net/sourceforge/filebot/format/MediaBindingBean.java
+++ source/net/sourceforge/filebot/format/MediaBindingBean.java
@@ -187,6 +187,26 @@ public class MediaBindingBean {
 	
 	@Define("vf")
 	public String getVideoFormat() {
+		// {def h = video.height as int; h > 720 ? 1080 : h > 480 ? 720 : 480}p
+		int height = Integer.parseInt(getMediaInfo(StreamKind.Video, 0, "Height"));
+		String vf = "";
+		
+		if (height > 720)
+			vf += 1080;
+		else if (height > 480)
+			vf += 720;
+		else if (height > 360)
+			vf += 480;
+		else
+			return null; // video too small
+			
+		// e.g. 720p
+		return vf + 'p'; // nobody actually wants files to be tagged as interlaced, e.g. 720i
+	}
+	
+	
+	@Define("hpi")
+	public String getExactVideoFormat() {
 		String height = getMediaInfo(StreamKind.Video, 0, "Height");
 		String scanType = getMediaInfo(StreamKind.Video, 0, "ScanType");
 		
