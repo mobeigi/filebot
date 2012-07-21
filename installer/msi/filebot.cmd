71d0bd478679dddbbe823b25d274bffeb92d1c4f
Reinhard Pointner <rednoah@users.noreply.github.com>
2012-07-21 20:30:22 +0000

    add connection timeout to all startup scripts: -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=25000

--- installer/msi/filebot.cmd
+++ installer/msi/filebot.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-java -Dapplication.deployment=msi -Dapplication.dir="%APPDATA%\FileBot" -Xmx256m -jar "%~dp0FileBot.jar" %*
+java -Dapplication.deployment=msi -Dapplication.dir="%APPDATA%\FileBot" -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=25000 -Xmx256m -jar "%~dp0FileBot.jar" %*
