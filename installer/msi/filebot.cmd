b4631951ab25f48435a38f736db24acbde10caae
Reinhard Pointner <rednoah@users.noreply.github.com>
2013-03-14 21:32:38 +0000

    * use launch4j for console executable instead of startup script

--- installer/msi/filebot.cmd
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-java -Xmx256m -DuseExtendedFileAttributes=true -Dapplication.dir="%APPDATA%\FileBot" -Dapplication.deployment=msi -Dapplication.analytics=true -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 "-Djna.library.path=%~dp0." "-Djava.library.path=%~dp0." -jar "%~dp0FileBot.jar" %*
