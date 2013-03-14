a902c00e46d8910767534752142ae167d8d73beb
Reinhard Pointner <rednoah@users.noreply.github.com>
2013-03-14 15:35:57 +0000

    * make the internal AcoustID api more flexible

--- source/net/sourceforge/filebot/web/AcoustID.java
+++ source/net/sourceforge/filebot/web/AcoustID.java
@@ -2,6 +2,7 @@
 package net.sourceforge.filebot.web;
 
 
+import static java.util.Collections.*;
 import static net.sourceforge.filebot.web.WebRequest.*;
 import static net.sourceforge.tuned.FileUtilities.*;
 
@@ -58,26 +59,39 @@ public class AcoustID implements MusicIdentificationService {
 		Map<File, AudioTrack> results = new LinkedHashMap<File, AudioTrack>();
 		
 		for (Map<String, String> fp : fpcalc(files)) {
-			results.put(new File(fp.get("FILE")), lookup(fp.get("DURATION"), fp.get("FINGERPRINT")));
+			results.put(new File(fp.get("FILE")), parse(lookup(fp.get("DURATION"), fp.get("FINGERPRINT"))));
 		}
 		return results;
 	}
 	
 	
-	public AudioTrack lookup(String duration, String fingerprint) throws IOException, InterruptedException, ParseException {
+	public String lookup(File file) throws Exception {
+		Map<String, String> fp = fpcalc(singleton(file)).get(0);
+		return lookup(fp.get("DURATION"), fp.get("FINGERPRINT"));
+	}
+	
+	
+	public String lookup(String duration, String fingerprint) throws IOException, InterruptedException, ParseException {
 		// http://api.acoustid.org/v2/lookup?client=8XaBELgH&meta=recordings+releasegroups+compress&duration=641&fingerprint=AQABz0qUkZK4oOfhL-CPc4e5C_wW2H2QH9uDL4cvoT8UNQ-eHtsE8cceeFJx-LiiHT-aPzhxoc-Opj_eI5d2hOFyMJRzfDk-QSsu7fBxqZDMHcfxPfDIoPWxv9C1o3yg44d_3Df2GJaUQeeR-cb2HfaPNsdxHj2PJnpwPMN3aPcEMzd-_MeB_Ej4D_CLP8ghHjkJv_jh_UDuQ8xnILwunPg6hF2R8HgzvLhxHVYP_ziJX0eKPnIE1UePMByDJyg7wz_6yELsB8n4oDmDa0Gv40hf6D3CE3_wH6HFaxCPUD9-hNeF5MfWEP3SCGym4-SxnXiGs0mRjEXD6fgl4LmKWrSChzzC33ge9PB3otyJMk-IVC6R8MTNwD9qKQ_CC8kPv4THzEGZS8GPI3x0iGVUxC1hRSizC5VzoamYDi-uR7iKPhGSI82PkiWeB_eHijvsaIWfBCWH5AjjCfVxZ1TQ3CvCTclGnEMfHbnZFA8pjD6KXwd__Cn-Y8e_I9cq6CR-4S9KLXqQcsxxoWh3eMxiHI6TIzyPv0M43YHz4yte-Cv-4D16Hv9F9C9SPUdyGtZRHV-OHEeeGD--BKcjVLOK_NCDXMfx44dzHEiOZ0Z44Rf6DH5R3uiPj4d_PKolJNyRJzyu4_CTD2WOvzjKH9GPb4cUP1Av9EuQd8fGCFee4JlRHi18xQh96NLxkCgfWFKOH6WGeoe4I3za4c5hTscTPEZTES1x8kE-9MQPjT8a8gh5fPgQZtqCFj9MDvp6fDx6NCd07bjx7MLR9AhtnFnQ70GjOcV0opmm4zpY3SOa7HiwdTtyHa6NC4e-HN-OfC5-OP_gLe2QDxfUCz_0w9l65HiPAz9-IaGOUA7-4MZ5CWFOlIfe4yUa6AiZGxf6w0fFxsjTOdC6Itbh4mGD63iPH9-RFy909XAMj7mC5_BvlDyO6kGTZKJxHUd4NDwuZUffw_5RMsde5CWkJAgXnDReNEaP6DTOQ65yaD88HoeX8fge-DSeHo9Qa8cTHc80I-_RoHxx_UHeBxrJw62Q34Kd7MEfpCcu6BLeB1ePw6OO4sOF_sHhmB504WWDZiEu8sKPpkcfCT9xfej0o0lr4T5yNJeOvjmu40w-TDmqHXmYgfFhFy_M7tD1o0cO_B2ms2j-ACEEQgQgAIwzTgAGmBIKIImNQAABwgQATAlhDGCCEIGIIM4BaBgwQBogEBIOESEIA8ARI5xAhxEFmAGAMCKAURKQQpQzRAAkCCBQEAKkQYIYIQQxCixCDADCABMAE0gpJIgyxhEDiCKCCIGAEIgJIQByAhFgGACCACMRQEyBAoxQiHiCBCFOECQFAIgAABR2QAgFjCDMA0AUMIoAIMChQghChASGEGeYEAIAIhgBSErnJPPEGWYAMgw05AhiiGHiBBBGGSCQcQgwRYJwhDDhgCSCSSEIQYwILoyAjAIigBFEUQK8gAYAQ5BCAAjkjCCAEEMZAUQAZQCjCCkpCgFMCCiIcVIAZZgilAQAiSHQECOcQAQIc4QClAHAjDDGkAGAMUoBgyhihgEChFCAAWEIEYwIJYwViAAlHCBIGEIEAEIQAoBwwgwiEBAEEEOoEwBY4wRwxAhBgAcKAESIQAwwIowRFhoBhAE
 		URL url = new URL("http://api.acoustid.org/v2/lookup?client=" + apikey + "&meta=recordings+releasegroups+releases+tracks+compress&duration=" + duration + "&fingerprint=" + fingerprint);
 		
 		Cache cache = Cache.getCache("web-datasource");
-		AudioTrack audioTrack = cache.get(url, AudioTrack.class);
-		if (audioTrack != null)
-			return audioTrack;
+		String response = cache.get(url, String.class);
+		if (response != null) {
+			return response;
+		}
 		
 		// respect rate limit
 		REQUEST_LIMIT.acquirePermit();
+		response = readAll(getReader(url.openConnection()));
+		cache.put(url, response);
 		
-		String response = readAll(getReader(url.openConnection()));
-		Map<?, ?> data = (Map<?, ?>) new JSONParser().parse(response);
+		return response;
+	}
+	
+	
+	public AudioTrack parse(String json) throws IOException, InterruptedException, ParseException {
+		Map<?, ?> data = (Map<?, ?>) new JSONParser().parse(json);
 		
 		if (!data.get("status").equals("ok")) {
 			throw new IOException("acoustid responded with error: " + data.get("status"));
@@ -90,7 +104,7 @@ public class AcoustID implements MusicIdentificationService {
 					String artist = (String) ((Map<?, ?>) ((List<?>) recording.get("artists")).get(0)).get("name");
 					String title = (String) recording.get("title");
 					
-					audioTrack = new AudioTrack(artist, title, null);
+					AudioTrack audioTrack = new AudioTrack(artist, title, null);
 					try {
 						Map<?, ?> releaseGroup = (Map<?, ?>) ((List<?>) recording.get("releasegroups")).get(0);
 						List<?> releases = (List<?>) releaseGroup.get("releases");
@@ -141,8 +155,6 @@ public class AcoustID implements MusicIdentificationService {
 						e.printStackTrace();
 						// allow album to be null
 					}
-					
-					cache.put(url, audioTrack);
 					return audioTrack;
 				} catch (Exception e) {
 				}
