86723cb3bf55dcef2171e39a49bc461f0a81487e
Reinhard Pointner <rednoah@users.noreply.github.com>
2013-04-30 11:20:28 +0000

    * make sure the user.home is never considered a movie folder

--- source/net/sourceforge/filebot/media/MediaDetection.java
+++ source/net/sourceforge/filebot/media/MediaDetection.java
@@ -632,6 +632,18 @@ public class MediaDetection {
 	
 	
 	public static File guessMovieFolder(File movieFile) throws Exception {
+		File folder = guessMovieFolderWithoutSanity(movieFile);
+		
+		// perform sanity checks
+		if (folder == null || folder.equals(new File(System.getProperty("user.home")))) {
+			return null;
+		}
+		
+		return folder;
+	}
+	
+	
+	private static File guessMovieFolderWithoutSanity(File movieFile) throws Exception {
 		// special case for folder mode 
 		if (movieFile.isDirectory()) {
 			File f = movieFile;
