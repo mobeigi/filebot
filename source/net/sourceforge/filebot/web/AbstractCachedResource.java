9eee85909b5992ea501397c203d000cabf079b2e
Reinhard Pointner <rednoah@users.noreply.github.com>
2013-11-12 12:29:24 +0000

    * fix long-term cache issues... int overflow... WTF!?!?

--- source/net/sourceforge/filebot/web/AbstractCachedResource.java
+++ source/net/sourceforge/filebot/web/AbstractCachedResource.java
@@ -12,9 +12,9 @@ import net.sf.ehcache.Element;
 
 public abstract class AbstractCachedResource<R, T extends Serializable> {
 
-	public static final long ONE_MONTH = 30 * 24 * 60 * 60 * 1000;
-	public static final long ONE_WEEK = 7 * 24 * 60 * 60 * 1000;
-	public static final long ONE_DAY = 24 * 60 * 60 * 1000;
+	public static final long ONE_MONTH = 30 * 24 * 60 * 60 * 1000L;
+	public static final long ONE_WEEK = 7 * 24 * 60 * 60 * 1000L;
+	public static final long ONE_DAY = 24 * 60 * 60 * 1000L;
 
 	private String resource;
 	private Class<T> type;
