f09aaf133cb4ff42628db1171eafcdcf44f23b55
Reinhard Pointner <rednoah@users.noreply.github.com>
2013-11-17 19:15:23 +0000

    * set internal extract calls in amc to --override skip

--- website/scripts/amc.groovy
+++ website/scripts/amc.groovy
@@ -95,7 +95,7 @@ def tempFiles = []
 input = input.flatten{ f ->
 	if (f.isArchive() || f.hasExtension('001')) {
 		def extractDir = new File(f.dir, f.nameWithoutExtension)
-		def extractFiles = extract(file: f, output: new File(extractDir, f.dir.name), conflict: 'override', filter: { it.isArchive() || it.isVideo() || it.isSubtitle() || (music && it.isAudio()) }, forceExtractAll: true) ?: []
+		def extractFiles = extract(file: f, output: new File(extractDir, f.dir.name), conflict: 'skip', filter: { it.isArchive() || it.isVideo() || it.isSubtitle() || (music && it.isAudio()) }, forceExtractAll: true) ?: []
 		tempFiles += extractDir
 		tempFiles += extractFiles
 		return extractFiles
