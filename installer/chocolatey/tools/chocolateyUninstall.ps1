ec73e35dc127903fda30c275ac22328d05f96f58
Reinhard Pointner <rednoah@users.noreply.github.com>
2013-12-26 07:36:48 +0000

    * properly support 'cuninst filebot'

---
+++ installer/chocolatey/tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+$app = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name = 'FileBot'"
+echo $app
+
+if ($app -eq $null) {
+	echo 'FileBot is not installed.'
+} else {
+	$app.Uninstall()
+	echo 'FileBot has been uninstalled.'
+}
