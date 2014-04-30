d18045a3837afe9759ec1fdd6fdaa18cd3ed1a6f
Reinhard Pointner <rednoah@users.noreply.github.com>
2014-04-30 04:36:31 +0000

    * fix dialog positioning issues

--- source/net/filebot/ui/subtitle/SubtitleDropTarget.java
+++ source/net/filebot/ui/subtitle/SubtitleDropTarget.java
@@ -212,10 +212,10 @@ abstract class SubtitleDropTarget extends JButton {
 			// initialize window properties
 			dialog.setIconImage(getImage(getIcon(DropAction.Accept)));
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setSize(1000, 575);
+			dialog.setSize(1000, 600);
 
 			// show dialog
-			dialog.setLocation(getOffsetLocation(dialog.getOwner()));
+			dialog.setLocationRelativeTo(dialog.getOwner());
 			dialog.setVisible(true);
 
 			return true;
