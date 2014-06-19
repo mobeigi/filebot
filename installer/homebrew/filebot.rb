7f67984bd77ad7663a0104001b9da018d32c8fa8
Reinhard Pointner <rednoah@users.noreply.github.com>
2014-06-19 07:32:23 +0000

    prepare homebrew filebot formula (the the filebot console command doesn't currently work due to issues in the startup script)

---
+++ installer/homebrew/filebot.rb
@@ -0,0 +1,20 @@
+require "formula"
+
+class Filebot < Formula
+  homepage "http://www.filebot.net/"
+  url "http://sourceforge.net/projects/filebot/files/filebot/FileBot_4.1/FileBot_4.1.app.tar.gz"
+  sha1 "1a0363b9a7bfa2bbd7d6c81d22a0c8e154c8ce81"
+  version "4.1"
+
+  def install
+    # Create .app bundle in prefix
+    (prefix/'FileBot.app').install Dir['*']
+
+    # Create filebot symlink in bin
+    bin.install_symlink prefix/'FileBot.app/Contents/MacOS/filebot.sh' => 'filebot'
+  end
+
+  test do
+    system "#{bin}/filebot", "-version"
+  end
+end
