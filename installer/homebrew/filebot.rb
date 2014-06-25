363c2c382f393277becf8b250da8fcde523547ab
Reinhard Pointner <rednoah@users.noreply.github.com>
2014-06-25 14:19:34 +0000

    convert to cask

--- installer/homebrew/filebot.rb
+++ installer/homebrew/filebot.rb
@@ -1,29 +1,11 @@
-require "formula"
-
-class Filebot < Formula
+class Filebot < Cask
   homepage "http://www.filebot.net/"
   url "https://downloads.sourceforge.net/project/filebot/filebot/HEAD/FileBot_4.1.1/FileBot_4.1.app.tar.gz"
-  sha1 "7a4e80f3d2bdb82afaa00e8350eba0c8888b8fe8"
+  sha256 "4aa3dd0532bd0266ec3bcb60553507dfb51c054a406fba95bd0c6d5c0dce7ec2"
   version "4.1.1"
 
-  def install
-    # Create .app bundle in prefix
-    (prefix/'FileBot.app').install Dir['*']
+  link "FileBot.app"
+  binary "FileBot.app/Contents/MacOS/filebot.sh", :target => "filebot"
 
-    # Create filebot symlink in bin
-    bin.install_symlink prefix/'FileBot.app/Contents/MacOS/filebot.sh' => 'filebot'
-  end
-
-  def caveats
-    "FileBot requires Java 8. Run `java -version` to verify."
-  end
-
-  def post_install
-    # Clearing cache and temporary files
-    system "#{bin}/filebot", "-clear-cache"
-  end
-
-  test do
-    system "#{bin}/filebot", "-version"
-  end
+  caveats "FileBot requires Java 8. Run `java -version` to verify."
 end
