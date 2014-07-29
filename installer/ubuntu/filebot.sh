3fea6a21de2ec8a62b7e717e5b2e7902d9171071
Reinhard Pointner <rednoah@users.noreply.github.com>
2014-07-29 11:21:15 +0000

    * experiment with embedding the JRE for the Ubuntu Software Center

--- installer/ubuntu/filebot.sh
+++ installer/ubuntu/filebot.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-java -Dunixfs=false -DuseGVFS=true -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.update=skip -Dapplication.deployment=usc -Dapplication.analytics=false -Dapplication.dir=$HOME/.filebot -Djava.io.tmpdir=$HOME/.filebot/temp -Djna.library.path=/opt/filebot -Djava.library.path=/opt/filebot -Dnet.filebot.AcoustID.fpcalc=fpcalc -jar /opt/filebot/FileBot.jar "$@"
+APP_ROOT="/opt/filebot"
+JAVA_CMD="$APP_ROOT/jre/bin/java"
+
+$JAVA_CMD -Dunixfs=false -DuseGVFS=true -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.update=skip -Dapplication.deployment=usc -Dapplication.analytics=false -Dnet.filebot.UserFiles.fileChooser=JavaFX "-Dapplication.dir=$HOME/.filebot" "-Djava.io.tmpdir=$HOME/.filebot/temp" "-Djna.library.path=$APP_ROOT" "-Djava.library.path=$APP_ROOT" "-Dnet.filebot.AcoustID.fpcalc=$APP_ROOT/fpcalc" -jar "$APP_ROOT/FileBot.jar" "$@"
