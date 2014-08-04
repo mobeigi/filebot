a6d1e7eddf0bd29f9f429b8338cbc2611625392a
Reinhard Pointner <rednoah@users.noreply.github.com>
2014-08-04 09:40:08 +0000

    * fix and fine-tune AcoustID lookup logic

--- source/net/filebot/web/AcoustIDClient.java
+++ source/net/filebot/web/AcoustIDClient.java
@@ -1,6 +1,6 @@
 package net.filebot.web;
 
-import static java.util.Collections.*;
+import static java.util.Arrays.*;
 import static net.filebot.web.WebRequest.*;
 
 import java.io.File;
@@ -10,6 +10,7 @@ import java.lang.ProcessBuilder.Redirect;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -25,8 +26,10 @@ import javax.swing.Icon;
 import net.filebot.Cache;
 import net.filebot.ResourceManager;
 
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import com.cedarsoftware.util.io.JsonObject;
+import com.cedarsoftware.util.io.JsonReader;
 
 public class AcoustIDClient implements MusicIdentificationService {
 
@@ -57,22 +60,26 @@ public class AcoustIDClient implements MusicIdentificationService {
 		Map<File, AudioTrack> results = new LinkedHashMap<File, AudioTrack>();
 
 		for (Map<String, String> fp : fpcalc(files)) {
-			results.put(new File(fp.get("FILE")), parse(lookup(fp.get("DURATION"), fp.get("FINGERPRINT"))));
+			File file = new File(fp.get("FILE"));
+			int duration = Integer.parseInt(fp.get("DURATION"));
+			String fingerprint = fp.get("FINGERPRINT");
+
+			if (duration > 10 && fingerprint != null) {
+				String response = lookup(duration, fingerprint);
+				if (response != null && response.length() > 0) {
+					results.put(file, parseResult(lookup(duration, fingerprint), duration));
+				}
+			}
 		}
 		return results;
 	}
 
-	public String lookup(File file) throws Exception {
-		Map<String, String> fp = fpcalc(singleton(file)).get(0);
-		return lookup(fp.get("DURATION"), fp.get("FINGERPRINT"));
-	}
-
-	public String lookup(String duration, String fingerprint) throws IOException, InterruptedException, ParseException {
+	public String lookup(int duration, String fingerprint) throws IOException, InterruptedException {
 		Map<String, String> postParam = new LinkedHashMap<String, String>();
-		postParam.put("duration", duration);
+		postParam.put("duration", String.valueOf(duration));
 		postParam.put("fingerprint", fingerprint);
 
-		String cacheKey = postParam.values().toString();
+		String cacheKey = postParam.toString();
 		Cache cache = getCache();
 		String response = cache.get(cacheKey, String.class);
 		if (response != null) {
@@ -83,7 +90,7 @@ public class AcoustIDClient implements MusicIdentificationService {
 		REQUEST_LIMIT.acquirePermit();
 
 		// http://api.acoustid.org/v2/lookup?client=8XaBELgH&meta=recordings+releasegroups+compress&duration=641&fingerprint=AQABz0qUkZK4oOfhL-CPc4e5C_wW2H2QH9uDL4cvoT8UNQ-eHtsE8cceeFJx-LiiHT-aPzhxoc-Opj_eI5d2hOFyMJRzfDk-QSsu7fBxqZDMHcfxPfDIoPWxv9C1o3yg44d_3Df2GJaUQeeR-cb2HfaPNsdxHj2PJnpwPMN3aPcEMzd-_MeB_Ej4D_CLP8ghHjkJv_jh_UDuQ8xnILwunPg6hF2R8HgzvLhxHVYP_ziJX0eKPnIE1UePMByDJyg7wz_6yELsB8n4oDmDa0Gv40hf6D3CE3_wH6HFaxCPUD9-hNeF5MfWEP3SCGym4-SxnXiGs0mRjEXD6fgl4LmKWrSChzzC33ge9PB3otyJMk-IVC6R8MTNwD9qKQ_CC8kPv4THzEGZS8GPI3x0iGVUxC1hRSizC5VzoamYDi-uR7iKPhGSI82PkiWeB_eHijvsaIWfBCWH5AjjCfVxZ1TQ3CvCTclGnEMfHbnZFA8pjD6KXwd__Cn-Y8e_I9cq6CR-4S9KLXqQcsxxoWh3eMxiHI6TIzyPv0M43YHz4yte-Cv-4D16Hv9F9C9SPUdyGtZRHV-OHEeeGD--BKcjVLOK_NCDXMfx44dzHEiOZ0Z44Rf6DH5R3uiPj4d_PKolJNyRJzyu4_CTD2WOvzjKH9GPb4cUP1Av9EuQd8fGCFee4JlRHi18xQh96NLxkCgfWFKOH6WGeoe4I3za4c5hTscTPEZTES1x8kE-9MQPjT8a8gh5fPgQZtqCFj9MDvp6fDx6NCd07bjx7MLR9AhtnFnQ70GjOcV0opmm4zpY3SOa7HiwdTtyHa6NC4e-HN-OfC5-OP_gLe2QDxfUCz_0w9l65HiPAz9-IaGOUA7-4MZ5CWFOlIfe4yUa6AiZGxf6w0fFxsjTOdC6Itbh4mGD63iPH9-RFy909XAMj7mC5_BvlDyO6kGTZKJxHUd4NDwuZUffw_5RMsde5CWkJAgXnDReNEaP6DTOQ65yaD88HoeX8fge-DSeHo9Qa8cTHc80I-_RoHxx_UHeBxrJw62Q34Kd7MEfpCcu6BLeB1ePw6OO4sOF_sHhmB504WWDZiEu8sKPpkcfCT9xfej0o0lr4T5yNJeOvjmu40w-TDmqHXmYgfFhFy_M7tD1o0cO_B2ms2j-ACEEQgQgAIwzTgAGmBIKIImNQAABwgQATAlhDGCCEIGIIM4BaBgwQBogEBIOESEIA8ARI5xAhxEFmAGAMCKAURKQQpQzRAAkCCBQEAKkQYIYIQQxCixCDADCABMAE0gpJIgyxhEDiCKCCIGAEIgJIQByAhFgGACCACMRQEyBAoxQiHiCBCFOECQFAIgAABR2QAgFjCDMA0AUMIoAIMChQghChASGEGeYEAIAIhgBSErnJPPEGWYAMgw05AhiiGHiBBBGGSCQcQgwRYJwhDDhgCSCSSEIQYwILoyAjAIigBFEUQK8gAYAQ5BCAAjkjCCAEEMZAUQAZQCjCCkpCgFMCCiIcVIAZZgilAQAiSHQECOcQAQIc4QClAHAjDDGkAGAMUoBgyhihgEChFCAAWEIEYwIJYwViAAlHCBIGEIEAEIQAoBwwgwiEBAEEEOoEwBY4wRwxAhBgAcKAESIQAwwIowRFhoBhAE
-		URL url = new URL("http://api.acoustid.org/v2/lookup?client=" + apikey + "&meta=recordings+releasegroups+compress");
+		URL url = new URL("http://api.acoustid.org/v2/lookup?client=" + apikey + "&meta=recordings+releases+releasegroups+tracks+compress");
 
 		// enable compression for request and response
 		Map<String, String> requestParam = new HashMap<String, String>();
@@ -97,25 +104,48 @@ public class AcoustIDClient implements MusicIdentificationService {
 		return response;
 	}
 
-	public AudioTrack parse(String json) throws IOException, InterruptedException, ParseException {
-		Map<?, ?> data = (Map<?, ?>) new JSONParser().parse(json);
+	private static Object[] array(Object node, String key) {
+		return ((JsonObject<?, ?>) ((Map<?, ?>) node).get(key)).getArray();
+	}
+
+	private static Map<?, ?> firstMap(Object node, String key) {
+		return (Map<?, ?>) array(node, key)[0];
+	}
+
+	private static Integer integer(Object node, String key) {
+		Object value = ((Map<?, ?>) node).get(key);
+		return value == null ? null : new Integer(value.toString());
+	}
+
+	public AudioTrack parseResult(String json, final int targetDuration) throws IOException, InterruptedException, ParseException {
+		Map<?, ?> data = (Map<?, ?>) JsonReader.jsonToMaps(json);
 
 		if (!data.get("status").equals("ok")) {
 			throw new IOException("acoustid responded with error: " + data.get("status"));
 		}
 		try {
-			for (Object result : (List<?>) data.get("results")) {
+			for (Object result : array(data, "results")) {
 				try {
-					Map<?, ?> recording = (Map<?, ?>) ((List<?>) ((Map<?, ?>) result).get("recordings")).get(0);
+					// pick most likely matching recording
+					Object[] recordings = array(result, "recordings");
+					sort(recordings, new Comparator<Object>() {
 
-					String artist = (String) ((Map<?, ?>) ((List<?>) recording.get("artists")).get(0)).get("name");
+						@Override
+						public int compare(Object o1, Object o2) {
+							Integer i1 = integer(o1, "duration");
+							Integer i2 = integer(o2, "duration");
+							return Double.compare(i1 == null ? Double.NaN : Math.abs(i1 - targetDuration), i2 == null ? Double.NaN : Math.abs(i2 - targetDuration));
+						}
+					});
+
+					Map<?, ?> recording = (Map<?, ?>) recordings[0];
+					String artist = (String) firstMap(recording, "artists").get("name");
 					String title = (String) recording.get("title");
 
 					AudioTrack audioTrack = new AudioTrack(artist, title, null);
 					try {
-						Map<?, ?> releaseGroup = (Map<?, ?>) ((List<?>) recording.get("releasegroups")).get(0);
-						List<?> releases = (List<?>) releaseGroup.get("releases");
-
+						Map<?, ?> releaseGroup = firstMap(recording, "releasegroups");
+						Object[] releases = array(releaseGroup, "releases");
 						if (releases != null) {
 							for (Object it : releases) {
 								try {
@@ -123,7 +153,7 @@ public class AcoustIDClient implements MusicIdentificationService {
 									Map<?, ?> release = (Map<?, ?>) it;
 									Map<?, ?> date = (Map<?, ?>) release.get("date");
 									try {
-										thisRelease.albumReleaseDate = new SimpleDate(Integer.parseInt(date.get("year").toString()), Integer.parseInt(date.get("month").toString()), Integer.parseInt(date.get("day").toString()));
+										thisRelease.albumReleaseDate = new SimpleDate(integer(date, "year"), integer(date, "month"), integer(date, "day"));
 									} catch (Exception e) {
 										// ignore
 									}
@@ -132,13 +162,13 @@ public class AcoustIDClient implements MusicIdentificationService {
 										continue;
 									}
 
-									Map<?, ?> medium = (Map<?, ?>) ((List<?>) release.get("mediums")).get(0);
-									thisRelease.mediumIndex = new Integer(medium.get("position").toString());
-									thisRelease.mediumCount = new Integer(release.get("medium_count").toString());
+									Map<?, ?> medium = firstMap(release, "mediums");
+									thisRelease.mediumIndex = integer(medium, "position");
+									thisRelease.mediumCount = integer(release, "medium_count");
 
-									Map<?, ?> track = (Map<?, ?>) ((List<?>) medium.get("tracks")).get(0);
-									thisRelease.trackIndex = new Integer(track.get("position").toString());
-									thisRelease.trackCount = new Integer(medium.get("track_count").toString());
+									Map<?, ?> track = firstMap(medium, "tracks");
+									thisRelease.trackIndex = integer(track, "position");
+									thisRelease.trackCount = integer(medium, "track_count");
 
 									try {
 										thisRelease.album = release.get("title").toString();
@@ -146,14 +176,15 @@ public class AcoustIDClient implements MusicIdentificationService {
 										thisRelease.album = (String) releaseGroup.get("title");
 									}
 									try {
-										thisRelease.albumArtist = (String) ((Map<?, ?>) ((List<?>) releaseGroup.get("artists")).get(0)).get("name");
+										thisRelease.albumArtist = (String) firstMap(releaseGroup, "artists").get("name");
 									} catch (Exception e) {
 										thisRelease.albumArtist = null;
 									}
 									thisRelease.trackTitle = (String) track.get("title");
 
 									if (!"Various Artists".equalsIgnoreCase(thisRelease.albumArtist) && (thisRelease.album == null || !thisRelease.album.contains("Greatest Hits"))) {
-										audioTrack = thisRelease;
+										// full info audio track
+										return thisRelease;
 									}
 								} catch (Exception e) {
 									Logger.getLogger(AcoustIDClient.class.getName()).log(Level.WARNING, e.toString(), e);
@@ -163,6 +194,8 @@ public class AcoustIDClient implements MusicIdentificationService {
 					} catch (Exception e) {
 						Logger.getLogger(AcoustIDClient.class.getName()).log(Level.WARNING, e.toString(), e);
 					}
+
+					// basic info audio track
 					return audioTrack;
 				} catch (Exception e) {
 					// ignore
