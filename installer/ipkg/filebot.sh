50f9e64f0d5c5326956b8ba120c8bd8879f0fb54
Reinhard Pointner <rednoah@users.noreply.github.com>
2014-10-26 17:48:52 +0000

    * make sure system encoding is always set to UTF-8 under all circumstances under all *nix systems

--- installer/ipkg/filebot.sh
+++ installer/ipkg/filebot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # force JVM language and encoding settings
-export LANG=en_US.utf8
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
 
 java -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding=UTF-8 -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Dapplication.deployment=ipkg -Dapplication.analytics=true -Duser.home=/opt/share/filebot/data -Dapplication.dir=/opt/share/filebot/data -Djava.io.tmpdir=/opt/share/filebot/data/temp -Djna.library.path=/opt/share/filebot -Djava.library.path=/opt/share/filebot -Dnet.filebot.AcoustID.fpcalc=/opt/share/filebot/fpcalc -jar /opt/share/filebot/FileBot.jar "$@"
--- installer/portable/filebot.sh
+++ installer/portable/filebot.sh
@@ -22,6 +22,7 @@ cd "$WORKING_DIR"
 
 
 # force JVM language and encoding settings
-export LANG=en_US.utf8
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
 
 java -Dunixfs=false -DuseGVFS=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding=UTF-8 -Djava.net.useSystemProxies=false -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.deployment=portable -Dapplication.analytics=true -Dnet.filebot.AcoustID.fpcalc=fpcalc "-Dapplication.dir=$APP_ROOT" "-Djava.io.tmpdir=$APP_ROOT/temp" "-Duser.home=$APP_ROOT" "-Djna.library.path=$APP_ROOT" "-Djava.library.path=$APP_ROOT" -Djava.util.prefs.PreferencesFactory=net.filebot.util.prefs.FilePreferencesFactory "-Dnet.filebot.util.prefs.file=$APP_ROOT/prefs.properties" -jar "$APP_ROOT/FileBot.jar" "$@"
