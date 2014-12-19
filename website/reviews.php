61866f708a64af9840b9fa3e1cd19e454a6c17f9
Reinhard Pointner <rednoah@users.noreply.github.com>
2014-12-19 16:38:53 +0000

    * let CF deal with integrating analytics into the page when served

--- website/reviews.php
+++ website/reviews.php
@@ -2,20 +2,7 @@
 	<head>
 		<title>Reviews</title>
 		<link rel="stylesheet" href="base.css" type="text/css" />
-		<link href='http://fonts.googleapis.com/css?family=Dawning+of+a+New+Day' rel='stylesheet' type='text/css'>
-		
-		<!-- google analytics -->
-		<script type="text/javascript">
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-25379256-1']);
-			_gaq.push(['_trackPageview']);
-			
-			(function() {
-				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
-		</script>
+		<link href='https://fonts.googleapis.com/css?family=Dawning+of+a+New+Day' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<div class="top"><small>Write a review for FileBot at <a href="https://sourceforge.net/projects/filebot/reviews/?sort=usefulness#review-form">SourceForge.net</a></small></div>
