57c74a3c92db4ad8ca0d04112c2a15abbd152c3b
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-03-28 18:37:38 +0000

    * clean up some launcher files and add default options for using the 7z executable

--- installer/deb/filebot.sh
+++ installer/deb/filebot.sh
@@ -1,2 +1,14 @@
-#!/bin/bash
-java -Dunixfs=false -DuseGVFS=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=false -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.deployment=deb -Dapplication.dir=$HOME/.filebot -Djava.io.tmpdir=$HOME/.filebot/temp -Djna.library.path=/usr/share/filebot -Djava.library.path=/usr/share/filebot -Dnet.filebot.AcoustID.fpcalc=/usr/share/filebot/fpcalc -jar /usr/share/filebot/FileBot.jar "$@"
+#!/bin/sh
+
+APP_ROOT="/usr/share/filebot"
+APP_DATA="$HOME/.filebot"
+
+# add APP_ROOT to LD_LIBRARY_PATH
+if [ ! -z "$LD_LIBRARY_PATH" ]
+then
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APP_ROOT"
+else
+    export LD_LIBRARY_PATH="$APP_ROOT"
+fi
+
+java -Dunixfs=false -DuseGVFS=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=false -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.deployment=deb "-Dapplication.dir=$APP_DATA" "-Djava.io.tmpdir=$APP_DATA/temp" "-Dnet.filebot.AcoustID.fpcalc=$APP_ROOT/fpcalc" -jar "$APP_ROOT/FileBot.jar" "$@"
--- installer/ubuntu/filebot.sh
+++ installer/ubuntu/filebot.sh
@@ -1,5 +1,18 @@
-#!/bin/bash
+#!/bin/sh
+
 APP_ROOT="/opt/filebot"
+APP_DATA="$HOME/.config/FileBot"
+APP_CACHE="$HOME/.cache/FileBot"
+
+# use embedded JRE
 JAVA_CMD="$APP_ROOT/jre/bin/java"
 
-$JAVA_CMD -Dunixfs=false -DuseGVFS=true -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.update=skip -Dapplication.deployment=usc -Dnet.filebot.UserFiles.fileChooser=JavaFX "-Dapplication.dir=$HOME/.config/FileBot" "-Dapplication.cache=$HOME/.cache/FileBot/ehcache.disk.store" "-Djava.io.tmpdir=$HOME/.cache/FileBot/java.io.tmpdir" "-Djna.library.path=$APP_ROOT" "-Djava.library.path=$APP_ROOT" "-Dnet.filebot.AcoustID.fpcalc=$APP_ROOT/fpcalc" -jar "$APP_ROOT/FileBot.jar" "$@"
+# add APP_ROOT to LD_LIBRARY_PATH
+if [ ! -z "$LD_LIBRARY_PATH" ]
+then
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APP_ROOT"
+else
+    export LD_LIBRARY_PATH="$APP_ROOT"
+fi
+
+"$JAVA_CMD" -Dunixfs=false -DuseGVFS=true -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.update=skip -Dapplication.deployment=usc -Dnet.filebot.UserFiles.fileChooser=JavaFX "-Dapplication.dir=$APP_DATA" "-Dapplication.cache=$APP_CACHE/ehcache.disk.store" "-Djava.io.tmpdir=$APP_CACHE/java.io.tmpdir" "-Dnet.filebot.AcoustID.fpcalc=$APP_ROOT/fpcalc" -jar "$APP_ROOT/FileBot.jar" "$@"
