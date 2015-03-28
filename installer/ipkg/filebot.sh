c129f726a9e508aded9dcf54d54621896fbc2bbe
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-03-28 18:14:58 +0000

    * clean up some launcher files and add default options for using the 7z executable

--- installer/ipkg/filebot.sh
+++ installer/ipkg/filebot.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+APP_ROOT="/opt/share/filebot"
+APP_DATA="$APP_ROOT/data/$USER"
+
+# add APP_ROOT to LD_LIBRARY_PATH
+if [ ! -z "$LD_LIBRARY_PATH" ]
+then
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APP_ROOT"
+else
+    export LD_LIBRARY_PATH="$APP_ROOT"
+fi
+
 # force JVM language and encoding settings
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
-java -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding=UTF-8 -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Dapplication.deployment=ipkg -Duser.home=/opt/share/filebot/data -Dapplication.dir=/opt/share/filebot/data -Djava.io.tmpdir=/opt/share/filebot/data/temp -Djna.library.path=/opt/share/filebot -Djava.library.path=/opt/share/filebot -Dnet.filebot.AcoustID.fpcalc=/opt/share/filebot/fpcalc -jar /opt/share/filebot/FileBot.jar "$@"
+java -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding=UTF-8 -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Dapplication.deployment=ipkg -Dnet.filebot.Archive.extractor=SevenZipExecutable "-Dnet.filebot.Archive.7z=7z" "-Dnet.filebot.AcoustID.fpcalc=fpcalc" "-Duser.home=$APP_DATA" "-Dapplication.dir=$APP_DATA" "-Djava.io.tmpdir=$APP_DATA/temp" -jar "$APP_ROOT/FileBot.jar" "$@"
