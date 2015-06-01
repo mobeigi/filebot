4863507cee27ae56612af4758502caee67a387eb
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-06-01 10:48:35 +0000

    * update website

--- website/download.php
+++ website/download.php
@@ -9,20 +9,19 @@ $file = 'undefined';
 if ($type == 'nsis') {
 	$folder = 'https://sourceforge.net/projects/filebot/files/filebot/HEAD';
 	$file = 'FileBot-setup.exe';
+} else if ($type == 'jar') {
+	$folder = 'https://sourceforge.net/projects/filebot/files/filebot/HEAD';
+	$file = 'FileBot.jar';
 } else if ($type == 'msi')
 	$file = 'FileBot_'.$version.'_'.$arch.'.msi';
 else if ($type == 'deb')
 	$file =  'filebot_'.$version.'_'.$arch.'.deb';
 else if ($type == 'app')
-	$file = 'FileBot_'.$version.'.app.tar.gz';
-else if ($type == 'jar')
-	$file = 'FileBot_'.$version.'.jar';
+	$file = 'FileBot_'.$version.'-brew.tar.bz2';
 else if ($type == 'portable')
 	$file = 'FileBot_'.$version.'-portable.zip';
 else if ($type == 'ipkg')
-	$file =  'filebot_'.$version.'_'.$arch.'.ipk';
-else if ($type == 'src')
-	$file = 'filebot-'.$version.'-src.zip';
+	$file = 'filebot_'.$version.'_noarch.ipk';
 
 
 $downloadPage = $folder.'/'.$file.'/download';
