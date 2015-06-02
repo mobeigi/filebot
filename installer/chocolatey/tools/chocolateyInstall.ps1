cb54b639db2272e0fdb411bde1bb1a2299fc010e
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-06-02 11:56:22 +0000

    * update chocolatey build with sha1 sums for all msi packages

--- installer/chocolatey/tools/chocolateyInstall.ps1
+++ installer/chocolatey/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $silent = '/quiet'
 $checksumType32 = 'sha1'
 $checksumType64 = 'sha1'
 
-$url32 = 'https://downloads.sourceforge.net/project/filebot/filebot/FileBot_/FileBot_@{version}_x86.msi'
+$url32 = 'https://downloads.sourceforge.net/project/filebot/filebot/FileBot_@{version}/FileBot_@{version}_x86.msi'
 $url64 = 'https://downloads.sourceforge.net/project/filebot/filebot/FileBot_@{version}/FileBot_@{version}_x64.msi'
 $checksum32 = '@{x86.msi.sha1}'
 $checksum64 = '@{x64.msi.sha1}'
