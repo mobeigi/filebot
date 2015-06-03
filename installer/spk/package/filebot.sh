b62b719700f439910626e7eee500ec56b2d9a032
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-06-03 19:27:30 +0000

    * use more variables to make editing easier

--- installer/spk/package/filebot.sh
+++ installer/spk/package/filebot.sh
@@ -24,6 +24,8 @@ cd "$WORKING_DIR"
 APP_DATA="$APP_ROOT/data/$USER"
 
 # add 3rd party packages to the library path by default
+SYNO_P7ZIP="/usr/syno/bin/7z"
+SYNO_FPCALC="/usr/local/chromaprint/bin/fpcalc"
 SYNO_LIBRARY_PATH="/usr/local/mediainfo/lib"
 
 # add APP_ROOT to LD_LIBRARY_PATH
@@ -38,4 +40,4 @@ fi
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
-java -Djava.awt.headless=true -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding=UTF-8 -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.deployment=spk -Dnet.filebot.Archive.extractor=SevenZipExecutable "-Dnet.filebot.Archive.7z=/usr/syno/bin/7z" "-Dnet.filebot.AcoustID.fpcalc=/usr/local/chromaprint/bin/fpcalc" "-Dapplication.dir=$APP_DATA" "-Djava.io.tmpdir=$APP_DATA/temp" "-Duser.home=$APP_DATA" -Djava.util.prefs.PreferencesFactory=net.filebot.util.prefs.FilePreferencesFactory "-Dnet.filebot.util.prefs.file=$APP_DATA/prefs.properties" -jar "$APP_ROOT/FileBot.jar" "$@"
+java -Djava.awt.headless=true -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding="UTF-8" -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.deployment=spk -Dnet.filebot.Archive.extractor=SevenZipExecutable -Dnet.filebot.Archive.7z="$SYNO_P7ZIP" -Dnet.filebot.AcoustID.fpcalc="$SYNO_FPCALC" -Dapplication.dir="$APP_DATA" -Djava.io.tmpdir="$APP_DATA/temp" -Duser.home="$APP_DATA" -Djava.util.prefs.PreferencesFactory=net.filebot.util.prefs.FilePreferencesFactory -Dnet.filebot.util.prefs.file="$APP_DATA/prefs.properties" -jar "$APP_ROOT/FileBot.jar" "$@"
