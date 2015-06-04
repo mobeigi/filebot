1f48628afd465fedb3c46135db04df39f1b4771b
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-06-04 10:41:43 +0000

    * use Apache Commons VFS2 with junrar plugin

--- installer/spk/package/filebot.sh
+++ installer/spk/package/filebot.sh
@@ -24,7 +24,6 @@ cd "$WORKING_DIR"
 APP_DATA="$APP_ROOT/data/$USER"
 
 # add 3rd party packages to the library path by default
-SYNO_P7ZIP="/usr/syno/bin/7z"
 SYNO_FPCALC="/usr/local/chromaprint/bin/fpcalc"
 SYNO_LIBRARY_PATH="/usr/local/mediainfo/lib"
 
@@ -40,4 +39,9 @@ fi
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
-java -Djava.awt.headless=true -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding="UTF-8" -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.deployment=spk -Dnet.filebot.Archive.extractor=SevenZipExecutable -Dnet.filebot.Archive.7z="$SYNO_P7ZIP" -Dnet.filebot.AcoustID.fpcalc="$SYNO_FPCALC" -Dapplication.dir="$APP_DATA" -Djava.io.tmpdir="$APP_DATA/temp" -Duser.home="$APP_DATA" -Djava.util.prefs.PreferencesFactory=net.filebot.util.prefs.FilePreferencesFactory -Dnet.filebot.util.prefs.file="$APP_DATA/prefs.properties" -jar "$APP_ROOT/FileBot.jar" "$@"
+# FileBot settings
+# EXTRACTOR="SevenZipNativeBindings"	# use the lib7-Zip-JBinding.so native library
+# EXTRACTOR="SevenZipExecutable"		# use the 7z executable
+EXTRACTOR="ApacheVFS"					# use Apache Commons VFS2 with junrar plugin
+
+java -Djava.awt.headless=true -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding="UTF-8" -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.deployment=spk -Dnet.filebot.Archive.extractor="$EXTRACTOR" -Dnet.filebot.AcoustID.fpcalc="$SYNO_FPCALC" -Dapplication.dir="$APP_DATA" -Djava.io.tmpdir="$APP_DATA/temp" -Duser.home="$APP_DATA" -Djava.util.prefs.PreferencesFactory=net.filebot.util.prefs.FilePreferencesFactory -Dnet.filebot.util.prefs.file="$APP_DATA/prefs.properties" -jar "$APP_ROOT/FileBot.jar" "$@"
