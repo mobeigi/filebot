042eb1379ca8806a491582b6cf6d9533d1372284
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-06-05 06:34:29 +0000

    * use sh template

--- installer/appbundle/filebot.sh
+++ installer/appbundle/filebot.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-PRG="${BASH_SOURCE[0]}"
+#!/bin/sh
+PRG="$0"
 
 # resolve relative symlinks
 while [ -h "$PRG" ] ; do
@@ -20,5 +20,4 @@ APP_ROOT=`cd "$PRG_DIR/../.." && pwd`
 # restore original working dir
 cd "$WORKING_DIR"
 
-
-java -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Dapple.awt.UIElement=true -Djna.nounpack=true -Dapplication.deployment=app "-Djna.library.path=$APP_ROOT/Contents/MacOS" "-Djava.library.path=$APP_ROOT/Contents/MacOS" "-Dnet.filebot.AcoustID.fpcalc=$APP_ROOT/Contents/MacOS/fpcalc" -jar "$APP_ROOT"/Contents/Java/* "$@"
+java -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Dapple.awt.UIElement=true -Djna.nounpack=true -Dapplication.deployment=app "-Djna.library.path=$APP_ROOT/Contents/MacOS" "-Djava.library.path=$APP_ROOT/Contents/MacOS" "-Dnet.filebot.AcoustID.fpcalc=$APP_ROOT/Contents/MacOS/fpcalc" -jar "$APP_ROOT"/Contents/Java/FileBot*.jar "$@"
