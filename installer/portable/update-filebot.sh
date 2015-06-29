0f5d92db761110de88e44a35564e0ee270d84afb
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-06-29 12:04:34 +0000

    * update filebot.sh as well

--- installer/portable/update-filebot.sh
+++ installer/portable/update-filebot.sh
@@ -3,13 +3,13 @@ PRG="$0"
 
 # resolve relative symlinks
 while [ -h "$PRG" ] ; do
-  ls=`ls -ld "$PRG"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    PRG="$link"
-  else
-    PRG="`dirname "$PRG"`/$link"
-  fi
+	ls=`ls -ld "$PRG"`
+	link=`expr "$ls" : '.*-> \(.*\)$'`
+	if expr "$link" : '/.*' > /dev/null; then
+		PRG="$link"
+	else
+		PRG="`dirname "$PRG"`/$link"
+	fi
 done
 
 # make it fully qualified
@@ -21,6 +21,13 @@ APP_ROOT=`cd "$PRG_DIR" && pwd`
 cd "$WORKING_DIR"
 
 
+
+# update core application files
+SH_FILE="$APP_ROOT/filebot.sh"
 JAR_FILE="$APP_ROOT/FileBot.jar"
+
 echo "Update $JAR_FILE"
 curl -L -o "$JAR_FILE" -z "$JAR_FILE" "https://downloads.sourceforge.net/project/filebot/filebot/HEAD/FileBot.jar"
+
+echo "Update $SH_FILE"
+curl -L -o "$SH_FILE" -z "$SH_FILE" "https://svn.code.sf.net/p/filebot/code/trunk/installer/portable/filebot.sh"
