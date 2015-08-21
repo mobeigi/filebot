4727de100625e00e2105955b48ad8e566845c7df
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-08-21 21:35:01 +0000

    * code sign NSIS installer

--- installer/nsis/make.cmd
+++ installer/nsis/make.cmd
@@ -1,8 +1,4 @@
 del FileBot-setup.exe
 makensis filebot.nsi
 
-del MyCA.pvk MyCA.cer MySPC.pvk MySPC.cer MySPC.pfx
-makecert -r -pe -n "CN=FileBot CA" -ss CA -sr CurrentUser -a sha256 -cy authority -sky signature -sv MyCA.pvk MyCA.cer
-makecert -pe -n "CN=FileBot NSIS" -a sha256 -cy end -sky signature -ic MyCA.cer -iv MyCA.pvk -sv MySPC.pvk MySPC.cer
-pvk2pfx -pvk MySPC.pvk -spc MySPC.cer -pfx MySPC.pfx
-signtool sign /v /f MySPC.pfx FileBot-setup.exe
+signtool sign /t http://timestamp.verisign.com/scripts/timstamp.dll /v /a FileBot-setup.exe
