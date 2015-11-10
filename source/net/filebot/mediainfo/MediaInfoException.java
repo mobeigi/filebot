e6dbd4538e09c63c117631061da8d77f3e414a01
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-11-10 06:19:50 +0000

    * improve MediaInfoException text

--- source/net/filebot/mediainfo/MediaInfoException.java
+++ source/net/filebot/mediainfo/MediaInfoException.java
@@ -5,13 +5,13 @@ import com.sun.jna.Platform;
 public class MediaInfoException extends RuntimeException {
 
 	public MediaInfoException(LinkageError e) {
-		super(getLinkageErrorMessage(e));
+		super(getLinkageErrorMessage(e), e);
 	}
 
 	private static String getLinkageErrorMessage(LinkageError e) {
 		String name = Platform.isWindows() ? "MediaInfo.dll" : Platform.isMac() ? "libmediainfo.dylib" : "libmediainfo.so";
-		String arch = System.getProperty("os.arch").toLowerCase().trim();
-		return String.format("Unable to load %s native library %s", arch, name);
+		String arch = System.getProperty("os.arch");
+		return String.format("Unable to load %s native library %s: %s", arch, name, e.getMessage());
 	}
 
 }
