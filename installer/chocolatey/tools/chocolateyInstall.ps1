6b05c3f0c66fbdd344458a8380a7894810f691fb
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-11-26 06:13:04 +0000

    * use SHA-256 for chocolatey package

--- installer/chocolatey/tools/chocolateyInstall.ps1
+++ installer/chocolatey/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$name = 'filebot'
 $type = 'msi'
 $silent = '/quiet'
-$algorithm = 'sha1'
 
-$url32 = 'https://downloads.sourceforge.net/project/filebot/filebot/FileBot_@{version}/FileBot_@{version}_x86.msi'
-$url64 = 'https://downloads.sourceforge.net/project/filebot/filebot/FileBot_@{version}/FileBot_@{version}_x64.msi'
-$checksum32 = '@{x86.msi.sha1}'
-$checksum64 = '@{x64.msi.sha1}'
+$url32 = 'http://downloads.sourceforge.net/project/filebot/filebot/FileBot_@{version}/FileBot_@{version}_x86.msi'
+$url64 = 'http://downloads.sourceforge.net/project/filebot/filebot/FileBot_@{version}/FileBot_@{version}_x64.msi'
+$checksum32 = '@{x86.msi.sha256}'
+$checksum64 = '@{x64.msi.sha256}'
+$algorithm = 'sha256'
 
 Install-ChocolateyPackage $name $type $silent $url32 $url64 -checksum $checksum32 -checksumType $algorithm -checksum64 $checksum64 -checksumType64 $algorithm
