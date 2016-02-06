e431a481205ef5322f049cfebb00f42a884aaf10
Reinhard Pointner <rednoah@users.noreply.github.com>
2016-02-06 12:23:53 +0000

    exclude anime movies from anime index

--- build-data/BuildData.groovy
+++ build-data/BuildData.groovy
@@ -360,7 +360,7 @@ def animeExcludes = new HashSet()
 
 // exclude anime movies from anime index
 new File('anime-list.xml').eachLine('UTF-8') {
-    if (it =~ /tvdbid="movie"/ || it =~ /defaulttvdbseason="0"/) {
+    if (it =~ /tvdbid="movie"/ || it =~ /imdbid="ttd\+"/) {
         animeExcludes << it.match(/anidbid="(\d+)"/).toInteger()
     }
 }
