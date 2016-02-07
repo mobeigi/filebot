e43e5f9f173341b604bd79c6dfc9794af0121c44
Reinhard Pointner <rednoah@users.noreply.github.com>
2016-02-07 09:42:56 +0000

    Launch via /usr/libexec/java_home to avoid `java` command not found / old version / etc issues

--- installer/appbundle/filebot.sh
+++ installer/appbundle/filebot.sh
@@ -20,4 +20,4 @@ APP_ROOT=`cd "$PRG_DIR/../.." && pwd`
 # restore original working dir
 cd "$WORKING_DIR"
 
-java $JAVA_OPTS -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Dapple.awt.UIElement=true -Djna.nounpack=true -Dapplication.deployment=app "-Djna.library.path=$APP_ROOT/Contents/MacOS" "-Djava.library.path=$APP_ROOT/Contents/MacOS" "-Dnet.filebot.AcoustID.fpcalc=$APP_ROOT/Contents/MacOS/fpcalc" -jar "$APP_ROOT"/Contents/Java/FileBot*.jar "$@"
+/usr/libexec/java_home --failfast --version '1.8+' --exec java $JAVA_OPTS -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Dapple.awt.UIElement=true -Djna.nounpack=true -Dapplication.deployment=app "-Djna.library.path=$APP_ROOT/Contents/MacOS" "-Djava.library.path=$APP_ROOT/Contents/MacOS" "-Dnet.filebot.AcoustID.fpcalc=$APP_ROOT/Contents/MacOS/fpcalc" -jar "$APP_ROOT"/Contents/Java/FileBot*.jar "$@"
