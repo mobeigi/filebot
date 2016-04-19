544389f43d7a4341dc529bef0f7df616d25a5364
Reinhard Pointner <rednoah@users.noreply.github.com>
2016-04-19 20:10:32 +0000

    Distribute FileBot Installer fia GitHub Releases

--- installer/nsis/make.cmd
+++
@@ -1,4 +0,0 @@
-del FileBot-setup.exe
-makensis filebot.nsi
-
-signtool sign /t http://timestamp.verisign.com/scripts/timstamp.dll /v /a FileBot-setup.exe
--- website/download.php
+++ website/download.php
@@ -8,7 +8,7 @@ $folder = $root.'FileBot_'.$version;
 $file = '';
 
 if ($type == 'nsis') {
-	$folder = $root.'LATEST';
+	$folder = 'https://github.com/filebot/filebot-installer/releases/download/v1.0/';
 	$file = 'FileBot-setup.exe';
 } else if ($type == 'jar') {
 	$folder = $root.'HEAD';
