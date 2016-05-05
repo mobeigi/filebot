e8295884ba9cefa20fbdd0b12c1b69acd7c146bf
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-05-05 21:50:47 +0800

    + Switch to new TheTVDB API

--- test/net/filebot/web/TheTVDBClientTest.java
+++ test/net/filebot/web/TheTVDBClientTest.java
@@ -1,22 +1,26 @@
 package net.filebot.web;
 
+import static java.util.Arrays.*;
 import static org.junit.Assert.*;
 
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Locale;
 
 import org.junit.Test;
 
-import net.filebot.web.TheTVDBClient.MirrorType;
-
 public class TheTVDBClientTest {
 
-	TheTVDBClient thetvdb = new TheTVDBClient("BA864DEE427E384A");
+	static TheTVDBClient thetvdb = new TheTVDBClient("BA864DEE427E384A");
 
 	SearchResult buffy = new SearchResult(70327, "Buffy the Vampire Slayer");
 	SearchResult wonderfalls = new SearchResult(78845, "Wonderfalls");
 	SearchResult firefly = new SearchResult(78874, "Firefly");
+
+	@Test
+	public void languages() throws Exception {
+		String[] languages = thetvdb.languages();
+		assertEquals("[zh, en, sv, no, da, fi, nl, de, it, es, fr, pl, hu, el, tr, ru, he, ja, pt, cs, sl, hr, ko]", asList(languages).toString());
+	}
 
 	@Test
 	public void search() throws Exception {
@@ -47,7 +51,7 @@ public class TheTVDBClientTest {
 	public void getEpisodeListAll() throws Exception {
 		List<Episode> list = thetvdb.getEpisodeList(buffy, SortOrder.Airdate, Locale.ENGLISH);
 
-		assertTrue(list.size() >= 144);
+		assertEquals(145, list.size());
 
 		// check ordinary episode
 		Episode first = list.get(0);
@@ -63,7 +67,7 @@ public class TheTVDBClientTest {
 		Episode last = list.get(list.size() - 1);
 		assertEquals("Buffy the Vampire Slayer", last.getSeriesName());
 		assertEquals("Unaired Pilot", last.getTitle());
-		assertEquals("1", last.getSeason().toString());
+		assertEquals(null, last.getSeason());
 		assertEquals(null, last.getEpisode());
 		assertEquals(null, last.getAbsolute());
 		assertEquals("1", last.getSpecial().toString());
@@ -104,15 +108,6 @@ public class TheTVDBClientTest {
 	}
 
 	@Test
-	public void resolveTypeMask() {
-		// no flags set
-		assertEquals(MirrorType.newSet(), MirrorType.fromTypeMask(0));
-
-		// all flags set
-		assertEquals(EnumSet.of(MirrorType.SEARCH, MirrorType.XML, MirrorType.BANNER), MirrorType.fromTypeMask(7));
-	}
-
-	@Test
 	public void lookupByID() throws Exception {
 		SearchResult series = thetvdb.lookupByID(78874, Locale.ENGLISH);
 		assertEquals("Firefly", series.getName());
@@ -128,29 +123,23 @@ public class TheTVDBClientTest {
 
 	@Test
 	public void getSeriesInfo() throws Exception {
-		TheTVDBSeriesInfo it = (TheTVDBSeriesInfo) thetvdb.getSeriesInfo(80348, Locale.ENGLISH);
+		SeriesInfo it = thetvdb.getSeriesInfo(80348, Locale.ENGLISH);
 
 		assertEquals(80348, it.getId(), 0);
-		assertEquals("TV-PG", it.getContentRating());
-		assertEquals("2007-09-24", it.getFirstAired().toString());
 		assertEquals("Action", it.getGenres().get(0));
-		assertEquals("tt0934814", it.getImdbId());
 		assertEquals("en", it.getLanguage());
-		assertEquals(987, it.getOverview().length());
 		assertEquals("45", it.getRuntime().toString());
 		assertEquals("Chuck", it.getName());
 	}
 
 	@Test
-	public void getBanner() throws Exception {
-		Artwork banner = thetvdb.getArtwork(buffy.getId(), "season", Locale.ROOT).stream().filter(it -> {
-			return it.getTags().contains("season") && it.getTags().contains("seasonwide") && it.getTags().contains("7") && it.getLanguage().equals("en");
-		}).findFirst().get();
+	public void getImages() throws Exception {
+		Artwork i = thetvdb.getArtwork(buffy.getId(), "fanart", Locale.ENGLISH).get(0);
 
-		assertEquals("season", banner.getTags().get(0));
-		assertEquals("seasonwide", banner.getTags().get(1));
-		assertEquals("http://thetvdb.com/banners/seasonswide/70327-7.jpg", banner.getUrl().toString());
-		assertEquals(99712, WebRequest.fetch(banner.getUrl()).remaining(), 0);
+		assertEquals("[fanart, 1280x720]", i.getTags().toString());
+		assertEquals("http://thetvdb.com/banners/fanart/original/70327-1.jpg", i.getUrl().toString());
+		assertTrue(i.matches("fanart", "1280x720"));
+		assertFalse(i.matches("fanart", "1280x720", "1"));
 	}
 
 }
