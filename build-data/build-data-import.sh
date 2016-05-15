b576216ffc43a8ddf71a7a59826e03e7ef08ad7d
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-05-16 00:26:22 +0800

    Make sure that indexed resources are resonable up-to-date

--- build-data/build-data-import.sh
+++ build-data/build-data-import.sh
@@ -22,9 +22,9 @@ fetch()
 fetch anidb.txt.gz 'http://anidb.net/api/anime-titles.dat.gz' +5
 fetch tvdb.zip 'http://thetvdb.com/api/58B4AA94C59AD656/updates/updates_all.zip' +5
 fetch omdb.zip 'http://beforethecode.com/projects/omdb/download.aspx?e=reinhard.pointner%40gmail.com&tsv=movies' +30
-fetch osdb.txt 'http://www.opensubtitles.org/addons/export_movie.php' +30
+fetch osdb.txt 'http://www.opensubtitles.org/addons/export_movie.php' +5
 
-fetch anime-list.xml 'https://raw.githubusercontent.com/ScudLee/anime-lists/master/anime-list.xml' +30
-fetch anime-movieset-list.xml 'https://raw.githubusercontent.com/ScudLee/anime-lists/master/anime-movieset-list.xml' +30
+fetch anime-list.xml 'https://raw.githubusercontent.com/ScudLee/anime-lists/master/anime-list.xml' +5
+fetch anime-movieset-list.xml 'https://raw.githubusercontent.com/ScudLee/anime-lists/master/anime-movieset-list.xml' +5
 
 echo 'DONE'
