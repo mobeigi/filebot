8bed1ba2df3b5bca8ecfb423d8059f41ed505d6d
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-05-23 14:17:40 +0800

    Types are good. Fixed `java.lang.ClassCastException: Cannot cast java.net.URI to [B` issues.
    @see https://www.filebot.net/forums/viewtopic.php?f=10&t=3808

--- source/net/filebot/CachedResource.java
+++ source/net/filebot/CachedResource.java
@@ -100,7 +100,7 @@ public class CachedResource<K, R> implements Resource<R> {
 				if (element == null || element.getObjectValue() == null) {
 					throw e;
 				}
-				return element.getObjectKey();
+				return element.getObjectValue();
 			}
 		});
 
