b14ec1768cf7fda280abce59e9a5befd164b07a6
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-06-14 22:06:10 +0800

    Refactor

--- installer/appbundle/filebot.sh
+++ installer/appbundle/filebot.sh
@@ -21,4 +21,4 @@ APP_ROOT=`cd "$PRG_DIR/../.." && pwd`
 cd "$WORKING_DIR"
 
 # start filebot
-/usr/libexec/java_home --failfast --version '1.8+' --exec java -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=true -Dapple.awt.UIElement=true -Djna.nounpack=true -Dapplication.deployment=app "-Djna.library.path=$APP_ROOT/Contents/MacOS" "-Djava.library.path=$APP_ROOT/Contents/MacOS" "-Dnet.filebot.AcoustID.fpcalc=$APP_ROOT/Contents/MacOS/fpcalc" $JAVA_OPTS -jar "$APP_ROOT"/Contents/Java/FileBot*.jar "$@"
+/usr/libexec/java_home --failfast --version "1.8+" --exec java -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Djava.net.useSystemProxies=true -Dapple.awt.UIElement=true -Djna.nounpack=true -Dapplication.deployment=app -Djna.library.path="$APP_ROOT/Contents/MacOS" -Djava.library.path="$APP_ROOT/Contents/MacOS" -Dnet.filebot.AcoustID.fpcalc="$APP_ROOT/Contents/MacOS/fpcalc" $JAVA_OPTS -jar "$APP_ROOT"/Contents/Java/*.jar "$@"
