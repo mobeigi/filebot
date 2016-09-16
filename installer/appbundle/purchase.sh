c4d5bf4123789805f273f9f8a9ec21ef5bcbd8d0
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-09-17 00:21:33 +0800

    Redirect to Mac App Store if brew command-line bundle is started via Finder

--- installer/appbundle/purchase.sh
+++ installer/appbundle/purchase.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/codesign -v /Applications/FileBot.app && /usr/local/bin/filebot || /usr/bin/open "https://itunes.apple.com/us/app/filebot/id905384638?mt=12&uo=6&at=1l3vupy&ct=darwin"
+/usr/bin/codesign -v /Applications/FileBot.app && /usr/local/bin/filebot || /usr/bin/open https://app.filebot.net/purchase/FileBot.app
