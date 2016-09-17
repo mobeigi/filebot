2036e2c4a08a1450282c8009b420d29772aaca11
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-09-17 12:14:54 +0800

    Set default donate amount to MAS price

--- installer/appbundle/purchase.sh
+++ installer/appbundle/purchase.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-/usr/bin/codesign -v /Applications/FileBot.app && /usr/local/bin/filebot || /usr/bin/open https://app.filebot.net/purchase/FileBot.app
+
+function validate {
+	/usr/bin/codesign -v /Applications/FileBot.app
+}
+
+function purchase {
+	/usr/bin/open https://app.filebot.net/purchase/FileBot.app
+}
+
+function help {
+	/usr/bin/open https://app.filebot.net/brew.html
+}
+
+function start {
+	/usr/local/bin/filebot
+}
+
+
+validate && (start || help) || purchase
