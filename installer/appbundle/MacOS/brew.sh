d8a2572db61cc06ed983f378a3bfb4fe2742c24b
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-09-19 14:17:09 +0800

    Include command-line helpers in app bundle builds

---
+++ installer/appbundle/MacOS/brew.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# brew requires Command Line Tools for Xcode
+xcode-select --print-path || sudo xcode-select --install
+
+# install brew if necessary
+brew info || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# update brew package index and update all packages
+brew update && brew upgrade
+
+# install latest JDK
+brew cask install java --force
+
+# install FileBot bundle to ~/Applications
+brew cask install filebot --force --appdir=~/Applications
