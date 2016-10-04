106fef7dd955a2d65a4baec7ccf22731799cd36b
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-10-05 03:34:42 +0800

    Make sure that last-resort 101 number parser does not pick digits from large numbers (e.g. 1234567890 => null instead of 123)
    @see https://www.filebot.net/forums/viewtopic.php?f=4&t=4194

--- source/net/filebot/similarity/SeasonEpisodeMatcher.java
+++ source/net/filebot/similarity/SeasonEpisodeMatcher.java
@@ -151,7 +151,7 @@ public class SeasonEpisodeMatcher {
 		};
 
 		// (last-resort) match patterns like 101, 102 (and greedily just grab the first)
-		Num101_SUBSTRING = new SeasonEpisodePattern(STRICT_SANITY, "([1-9]{1})(\\d{2}).*") {
+		Num101_SUBSTRING = new SeasonEpisodePattern(STRICT_SANITY, "(?<!\\p{Digit})(\\d{1})(\\d{2})(?!\\p{Digit})(.*)") {
 
 			@Override
 			protected Collection<SxE> process(MatchResult match) {
