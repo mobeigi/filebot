5ca51e1d7d1dc5a8d49d1dc919d8e5d1cd6f4d0c
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-10-08 23:56:29 +0800

    Minor optimizations

--- source/net/filebot/web/MultiEpisode.java
+++ source/net/filebot/web/MultiEpisode.java
@@ -2,6 +2,7 @@ package net.filebot.web;
 
 import static java.util.Arrays.*;
 import static java.util.Collections.*;
+import static java.util.stream.Collectors.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,17 +16,51 @@ public class MultiEpisode extends Episode {
 	}
 
 	public MultiEpisode(Episode... episodes) {
-		super(episodes[0]);
 		this.episodes = episodes.clone();
 	}
 
 	public MultiEpisode(List<Episode> episodes) {
-		super(episodes.get(0));
 		this.episodes = episodes.toArray(new Episode[0]);
 	}
 
 	public List<Episode> getEpisodes() {
 		return unmodifiableList(asList(episodes));
+	}
+
+	public String getSeriesName() {
+		return episodes[0].getSeriesName();
+	}
+
+	public Integer getEpisode() {
+		return episodes[0].getEpisode();
+	}
+
+	public Integer getSeason() {
+		return episodes[0].getSeason();
+	}
+
+	public String getTitle() {
+		return EpisodeFormat.SeasonEpisode.formatMultiTitle(getEpisodes());
+	}
+
+	public Integer getAbsolute() {
+		return episodes[0].getAbsolute();
+	}
+
+	public Integer getSpecial() {
+		return episodes[0].getSpecial();
+	}
+
+	public SimpleDate getAirdate() {
+		return episodes[0].getAirdate();
+	}
+
+	public SeriesInfo getSeriesInfo() {
+		return episodes[0].getSeriesInfo();
+	}
+
+	public List<Integer> getNumbers() {
+		return stream(episodes).flatMap(e -> e.getNumbers().stream()).collect(toList());
 	}
 
 	@Override
