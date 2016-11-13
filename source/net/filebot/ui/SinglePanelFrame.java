1d997e556e04f8dce47fbb540afb5af4dc365876
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-11-13 16:55:43 +0800

    Don't show version in single panel frame window header

--- source/net/filebot/ui/SinglePanelFrame.java
+++ source/net/filebot/ui/SinglePanelFrame.java
@@ -14,7 +14,7 @@ import net.miginfocom.swing.MigLayout;
 public class SinglePanelFrame extends JFrame {
 
 	public SinglePanelFrame(PanelBuilder builder) {
-		super(String.format("%s %s %s", getApplicationName(), builder.getName(), getApplicationVersion()));
+		super(String.format("%s %s", getApplicationName(), builder.getName()));
 		JComponent panel = builder.create();
 
 		JComponent c = (JComponent) getContentPane();
