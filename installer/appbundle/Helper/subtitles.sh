fa389c0490c981c481a03955ccda4492b62b56eb
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-11-16 00:37:10 +0800

    open --reveal "$APP"

--- installer/appbundle/Helper/subtitles.sh
+++ installer/appbundle/Helper/subtitles.sh
@@ -3,6 +3,7 @@
 PKG_NAME="FileBot_Subtitles_Installer.pkg"
 PKG_URL="https://app.filebot.net/files/$PKG_NAME"
 PKG="/tmp/$PKG_NAME"
+APP="/Applications/FileBot Subtitles.app"
 
 function fetch {
 	curl --location --output "$1" --time-cond "$1" --retry 5 "$2"
@@ -16,4 +17,4 @@ function install {
 	sudo installer -verbose -package "$1" -target LocalSystem
 }
 
-fetch "$PKG" "$PKG_URL" && verify "$PKG" && install "$PKG"
+fetch "$PKG" "$PKG_URL" && verify "$PKG" && install "$PKG" && open --reveal "$APP"
