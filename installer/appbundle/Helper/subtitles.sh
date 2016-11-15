887957ea6f11c4d63262bf679d2bc34ac77bd274
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-11-16 00:14:38 +0800

    pkgutil --check-signature "$1" && spctl --assess --verbose --ignore-cache --no-cache --type install "$1"

--- installer/appbundle/Helper/subtitles.sh
+++ installer/appbundle/Helper/subtitles.sh
@@ -4,4 +4,16 @@ PKG_NAME="FileBot_Subtitles_Installer.pkg"
 PKG_URL="https://app.filebot.net/files/$PKG_NAME"
 PKG="/tmp/$PKG_NAME"
 
-curl -L -o "$PKG" -z "$PKG" --retry 5 "$PKG_URL" && spctl -a -v --type install "$PKG" && sudo installer -verbose -pkg "$PKG" -target LocalSystem
+function fetch {
+	curl --location --output "$1" --time-cond "$1" --retry 5 "$2"
+}
+
+function verify {
+	pkgutil --check-signature "$1" && spctl --assess --verbose --ignore-cache --no-cache --type install "$1"
+}
+
+function install {
+	sudo installer -verbose -package "$1" -target LocalSystem
+}
+
+fetch "$PKG" "$PKG_URL" && verify "$PKG" && install "$PKG"
