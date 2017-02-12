74ead4e36c27962755c0864e918267f1baf7c4e0
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-02-12 21:54:00 +0800

    Make sure to debug log network errors

--- source/net/filebot/CachedResource.java
+++ source/net/filebot/CachedResource.java
@@ -128,7 +128,7 @@ public class CachedResource<K, R> implements Resource<R> {
 				throw e;
 			}
 
-			debug.finest(format("Fetch failed: Try again in %d seconds (%d more) => %s", retryWaitTime.getSeconds(), retryCount, e));
+			debug.warning(format("Fetch failed: Try again in %d seconds (%d more) => %s", retryWaitTime.getSeconds(), retryCount, e));
 			Thread.sleep(retryWaitTime.toMillis());
 			return retry(callable, retryCount - 1, retryWaitTime.multipliedBy(2));
 		}
