498b175fe98ea380312b539cf9e570ac191fcac0
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-03-03 17:36:22 +0800

    Allow rename(action) parameter to override the --action command-line option
    See https://www.filebot.net/forums/viewtopic.php?f=4&t=4801

--- source/net/filebot/cli/ScriptShellBaseClass.java
+++ source/net/filebot/cli/ScriptShellBaseClass.java
@@ -339,7 +339,7 @@ public abstract class ScriptShellBaseClass extends Script {
 
 		try {
 			if (files.size() > 0) {
-				return getCLI().rename(files, args.getRenameAction(), args.getConflictAction(), args.getAbsoluteOutputFolder(), args.getExpressionFileFormat(), args.getDatasource(), args.getSearchQuery(), args.getSortOrder(), args.getExpressionFilter(), args.getLanguage().getLocale(), args.isStrict());
+				return getCLI().rename(files, action, args.getConflictAction(), args.getAbsoluteOutputFolder(), args.getExpressionFileFormat(), args.getDatasource(), args.getSearchQuery(), args.getSortOrder(), args.getExpressionFilter(), args.getLanguage().getLocale(), args.isStrict());
 			}
 
 			if (map.size() > 0) {
