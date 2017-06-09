e7b464b5ab73e82a44e732a432a3a01e1054d67c
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-06-09 15:51:52 +0800

    Deprecate chocolatey builds

--- installer/chocolatey/tools/chocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$app = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name = '@{application.name}'"
-
-if ($app -eq $null) {
-	echo 'FileBot is not installed.'
-} else {
-	$app.Uninstall()
-	echo 'FileBot has been uninstalled.'
-}
