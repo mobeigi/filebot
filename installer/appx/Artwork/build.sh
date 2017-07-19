3cee3b3ca89005a3f6635b01b47bb84c19ce26a5
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-07-19 20:45:25 +0800

    Require logos in 300x300 1440x2160 2160x2160

--- installer/appx/Artwork/build.sh
+++ installer/appx/Artwork/build.sh
@@ -2,6 +2,6 @@
 
 LOGO='artwork_4096x4096.png'
 
-for SIZE in 300x300 1440x2160 2160x2160; do
+for SIZE in 71x71 150x150 300x300 1440x2160 2160x2160; do
 	convert -verbose $LOGO -resize $SIZE -gravity center -background transparent -extent $SIZE "artwork_$SIZE.png" 
 done
