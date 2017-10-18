6ed5ffd3e700a90922e4d3b49c2bdbcc1e53d125
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-10-18 23:38:55 +0200

    Do not open Getting Started dialog within app (to avoid JavaFX webkit issues)

--- source/net/filebot/ui/GettingStartedStage.java
+++ source/net/filebot/ui/GettingStartedStage.java
@@ -31,7 +31,7 @@ public class GettingStartedStage {
 	public static void start() {
 		invokeJavaFX(() -> {
 			// libjfxwebkit.dylib cannot be deployed on the MAS due to deprecated dependencies
-			if (isMacSandbox()) {
+			if (isAppStore()) {
 				ask();
 			} else {
 				create().show();
